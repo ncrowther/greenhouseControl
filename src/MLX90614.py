c8efbdd211a0b7600e02401669a755f11777bd0b
Bamoe <bamoe@ibm.com>
2025-02-13 09:54:52 +0000

    Update MLX90614.py

--- src/MLX90614.py
+++ src/MLX90614.py
@@ -5,14 +5,17 @@
 `MLX90614`
 ================================================================================
 
-MicroPython Driver for the Sensirion MLX90614 Temperature Sensor
+Raspberry Pico Driver for the Sensirion MLX90614 Temperature Sensor
 
 
-* Author(s): Jose D. Montoya
+* Author(s): Nigel T. Crowther
+* Date: 13-Feb-2025
 
 
 """
-
+import sys
+import math
+from machine import Pin, PWM, ADC, I2C
 import time
 import struct
 from micropython import const
@@ -21,6 +24,41 @@ from micropython import const
 __version__ = "0.0.0+auto.0"
 __repo__ = "https://github.com/jposada202020/MicroPython_MLX90614.git"
 
+
+class MLX90614:
+    """Driver for the MLX90614 Sensor connected over I2C.
+
+    :param ~machine.I2C i2c: The I2C bus the MLX90614 is connected to.
+    :param int address: The I2C device address. Defaults to :const:`0x40`
+
+    :raises RuntimeError: if the sensor is not found
+
+    **Quickstart: Importing and using the device**
+
+    Here is an example of using the :class:`MLX90614` class.
+    First you will need to import the libraries to use the sensor
+
+    .. code-block:: python
+
+        from machine import Pin, I2C
+        from MLX90614 import MLX90614
+
+    Once this is done you can define your `machine.I2C` object and define your sensor object
+
+    .. code-block:: python
+
+        i2c = I2C(1, sda=Pin(2), scl=Pin(3))
+        probe = MLX90614.MLX90614(i2c)
+
+    Now you have access to the attributes
+
+    .. code-block:: python
+
+        airTemperature = probe.get_amb_temp()
+        objectTemperature = probe.get_obj_temp()
+
+    """
+    
     MLX90614_RAWIR1=0x04
     MLX90614_RAWIR2=0x05
     MLX90614_TA=0x06
@@ -40,44 +78,9 @@ __repo__ = "https://github.com/jposada202020/MicroPython_MLX90614.git"
     MLX90614_ID4=0x3F
 
     comm_retries = 5
-    comm_sleep_amount = 0.1
+    comm_sleep_amount = 0.1    
 
-
-class MLX90614:
-    """Driver for the MLX90614 Sensor connected over I2C.
-
-    :param ~machine.I2C i2c: The I2C bus the MLX90614 is connected to.
-    :param int address: The I2C device address. Defaults to :const:`0x40`
-
-    :raises RuntimeError: if the sensor is not found
-
-    **Quickstart: Importing and using the device**
-
-    Here is an example of using the :class:`MLX90614` class.
-    First you will need to import the libraries to use the sensor
-
-    .. code-block:: python
-
-        from machine import Pin, I2C
-        from micropython_MLX90614 import MLX90614
-
-    Once this is done you can define your `machine.I2C` object and define your sensor object
-
-    .. code-block:: python
-
-        i2c = I2C(1, sda=Pin(2), scl=Pin(3))
-        probe = MLX90614.MLX90614(i2c)
-
-    Now you have access to the attributes
-
-    .. code-block:: python
-
-        airTemperature = probe.get_amb_temp()
-        objectTemperature = probe.get_obj_temp()
-
-    """
-
-    def __init__(self, i2c, address: int = 0x40) -> None:
+    def __init__(self) -> None:
         
         # setup the I2C communication for the SHT20 sensor
         #  I2C Pins
@@ -85,22 +88,43 @@ class MLX90614:
         I2C_SDA = 6
         I2C_SCL = 7        
 
-        i2c = I2C(I2C_PORT, scl=Pin(I2C_SCL), sda=Pin(I2C_SDA))
+        i2c = I2C(I2C_PORT, scl=Pin(I2C_SCL), sda=Pin(I2C_SDA), freq=100000)
                                                       
-        addr = i2c.scan()
-
+        scan = i2c.scan()
+        print("I2C scan:" + str(scan))
+        
         addr = i2c.scan()[0]
         print("**************************" + str(addr))
 
         self._i2c = i2c
-        self._address = address     
+        self._address = addr
 
-    def read_reg(regAddr):     
-        buf = i2c.readfrom_mem(self._address, regAddr, 2) # read 2 bytes from memory address regAddr of peripheral i2cAddr
-        return buf
+    
+    def read_reg(self, reg_addr):
+        err = None
+        for i in range(self.comm_retries):
+            try:
+                return self._i2c.readfrom_mem(self._address, reg_addr, 2)
+            except Exception as e:
+                err = e
+                #"Rate limiting" - sleeping to prevent problems with sensor
+                #when requesting data too quickly
+                print(err)
+                time.sleep(self.comm_sleep_amount)
+        #By this time, we made a couple requests and the sensor didn't respond
+        #(judging by the fact we haven't returned from this function yet)
+        #So let's just re-raise the last IOError we got
+        raise err    
+
 
     def data_to_temp(self, data):
-        temp = (data*0.02) - 273.15
+        
+        int_val = int.from_bytes(data, "little")
+ 
+        # printing int equivalent
+        #print(int_val)
+    
+        temp = (int_val*0.02) - 273.15
         return temp
 
     def get_amb_temp(self):
@@ -112,8 +136,22 @@ class MLX90614:
         return self.data_to_temp(data)
     
 if __name__ == "__main__":
+    
     sensor = MLX90614()
-    while True:    
-        print("Ambient Temp:",round(sensor.get_amb_temp(),2),"C")
-        print(" Object Temp:",round(sensor.get_obj_temp(),2),"C\n")
+    
+    time.sleep(1)
+        
+    # Read device ID to make sure that we can communicate with the ADXL343
+    data = sensor.read_reg(sensor.MLX90614_TA)
+    print(data)
+    
+
+    while True:
+        ambientTemp = round(sensor.get_amb_temp(),2)
+        objectTemp = round(sensor.get_obj_temp(),2)
+        diff =  objectTemp - ambientTemp
+        
+        print("Ambient Temp:",ambientTemp,"C")
+        print(" Object Temp:", objectTemp,"C")
+        print(" Diff:", diff,"C\n")
         time.sleep(0.5)    
