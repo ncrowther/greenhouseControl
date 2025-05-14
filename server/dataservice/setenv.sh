d19eda73ac754fc5701a86867c63dcd49f63347e
Nigel Crowther <ncrowther@uk.ibm.com>
2025-05-14 22:21:13 +0100

    update camera handling

--- server/dataservice/setenv.sh
+++ server/dataservice/setenv.sh
@@ -1,3 +1,6 @@
+# Execute form command line with command:
+# source ./setenv.sh 
+
 export CLOUDANT_URL=https://3ac2a61f-2417-4dc1-b80a-fc2fe0981b7a-bluemix.cloudantnosqldb.appdomain.cloud
 export CLOUDANT_APIKEY=czz8OBUUT3ZWVeHosqVNQX8fMlTW0BYUAz5cJHrKlPLJ
 export CONFIG_DB_NAME=greenhouseconfig
