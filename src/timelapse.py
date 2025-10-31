315726091ec97c0de9c57b0a6cdb971b5367fe03
NigelCrowther <ncrowther@uk.ibm.com>
2025-10-31 10:51:29 +0000

    feat: update timelapse.py

--- src/timelapse.py
+++ src/timelapse.py
@@ -1,3 +1,10 @@
+# Timelapse camera for Raspbery Pi
+# Author: Nigel T. Crowther
+# Date: 31-Oct-2025
+#
+# Set CAMERA_NUMBER to uniquely identify camera image
+# For remote maintenance via SSH, see https://phoenixnap.com/kb/enable-ssh-raspberry-pi
+#
 from time import sleep, time
 from picamera2 import Picamera2, Preview
 import base64
@@ -7,6 +14,13 @@ from PIL import Image
 from PIL import ImageDraw
 import socket
 
+CAMERA_NUMBER = 3
+GREENHOUSE_SERVER_URL =  'https://foxhound-hip-initially.ngrok-free.app'
+BASE_DIR = '/home/ncrowther/Pictures/greenhouse'
+WAIT_TIME =  60 * 15  # Photo every fifteen mins
+NO_IMAGE = 8000 # Image size below which it is discarded
+
+# Check internet connection
 def checkInternet():
     try:
         res = socket.getaddrinfo('google.com',80)
@@ -14,41 +28,50 @@ def checkInternet():
     except:
         return False
 
-hostname = socket.gethostname()
-#IPAddr = socket.gethostbyname(hostname)
-IPAddr = hostname
-
-GREENHOUSE_SERVER_URL =  'https://foxhound-hip-initially.ngrok-free.app'
-BASE_DIR = '/home/ncrowther/Pictures/greenhouse'
-WAIT_TIME =  60 * 15  # every fifteen mins
-NO_IMAGE = 8000
-
-picam2 = Picamera2()
-
-camera_config = picam2.create_preview_configuration(main={"size":(320,240), "format": "RGB888"})
-picam2.configure(camera_config)
-picam2.start_preview(Preview.NULL)
-
-frame = 1
-
-while True:
-
-    picam2.start()
-
+# Function to wait for internet connection
+def waitForInternet():
     while not(checkInternet()):
-        sleep(5)
+        sleep(1)
 
-    print("Frame " + str(frame))
-
-    # Generate timestamp
+def generateTimestamp():
     ts = datetime.now().timestamp()
     date_time = datetime.fromtimestamp(ts)
     timestamp = date_time.strftime("%Y-%m-%d %H:%M:%S")
     timestamp = str(timestamp)[0:19]
-    print(timestamp)
+    return timestamp        
+        
+def postPhoto(image, frame, timestamp):
+    image_64 = base64.b64encode(open(image, "rb").read())
+
+    imageLen = len(image_64)
+    print("Image length: {}".format(imageLen))
+
+    # Discard if the image is less than a defined size
+    if (imageLen < NO_IMAGE):
+        print("Discarding")
+        return
+
+    # Wait until WIFI connected
+    waitForInternet()
+
+    # Post photo to server
+    url = GREENHOUSE_SERVER_URL + '/photo'
+    myobj = {'_id': frame, 'cam': CAMERA_NUMBER, 'photo': image_64, 'timestamp': timestamp}
+    try:
+        res = requests.post(url, data=myobj)
+        print("POST:" + str(res))
+    except Exception as e:
+        print(f'Failed to send photo. {type(e)}: e')
+        print("Message: {}".format(e))
+
+def takePhoto(timestamp):
+
+    # Start camera and wait
+    picam2.start()      
+    sleep(5)  
 
     # Create image file path
-    image = BASE_DIR + str(timestamp) + '.jpg'
+    image = BASE_DIR + str(timestamp) + '.jpg'    
 
     picam2.capture_file(image)
 
@@ -64,34 +87,37 @@ while True:
     #Draw a filled black rectangle
     I1.rectangle(xy, outline="white", fill="black", width = 1)
 
-    # Add timestamp to the image
+    # Add timestamp and hostname to the image
     I1.text((4, 4), timestamp, fill="white")
     I1.text((4, 14), hostname, fill="white")
-
-    # Display edited image
-    #img.show()
 
     # Save the edited image
     img.save(image)
 
+    return image
+
+
+hostname = socket.gethostname()
+picam2 = Picamera2()
+camera_config = picam2.create_preview_configuration(main={"size":(320,240), "format": "RGB888"})
+picam2.configure(camera_config)
+picam2.start_preview(Preview.NULL)
+
+frame = 1
+
+while True:
+                
+    print("Frame " + str(frame))
+
+    # Generate timestamp
+    timestamp = generateTimestamp()
+    print(timestamp)
+
+    image = takePhoto(timestamp)
+
     picam2.stop()
 
-    image_64 = base64.b64encode(open(image,"rb").read())
-
-    imageLen = len(image_64)
-    print("Image length: {}".format(imageLen))
-
-    if (imageLen < NO_IMAGE):
-        print("Discarding")
-    else:
-        url = GREENHOUSE_SERVER_URL + '/photo'
-        myobj = {'_id': frame, 'cam': 1 , 'photo': image_64, 'timestamp': timestamp}
-        try:
-            res = requests.post(url, data = myobj)
-            print("POST:" + str(res))
-        except Exception as e:
-            print( f'Failed to send photo. {type(e)}: e')
-            print( "Message: {}".format(e))
+    postPhoto(image, frame, timestamp)
 
     sleep(WAIT_TIME)
 
